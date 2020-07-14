dc14a907ab1fc508e28fea472d96053b24108304
Stefan Rohrbacher <stefan.rohrbacher97@gmail.com>
2020-07-14 10:40:14 +0200

    code cleanup & automatic snapshots

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,29 +10,35 @@
   ];
 
   # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.copyKernels = true;
-  boot.loader.grub.configurationLimit = 20;
-  boot.loader.grub.zfsSupport = true;
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    copyKernels = true;
+    configurationLimit = 20;
+    zfsSupport = true;
+    devices = [ "/dev/sdc" ];
+  };
+
   boot.supportedFilesystems = [ "zfs" "xfs" "ext4" ];
   boot.zfs.enableUnstable = true;
   boot.kernelParams = [
     "elevator=none"
     "intel_iommu=on"
-    "amd_iommu=on" ];
-  boot.loader.grub.devices = [ "/dev/sda" "/dev/sdb" ];
+   # "amd_iommu=on"
+  ];
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos";
   networking.hostId = "994d9128";
-  networking.networkmanager.enable = true;
+
+  networking.networkmanager = { enable = true; };
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # The global use DHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  networking.interfaces.eth0.useDHCP = true;
+  networking.interfaces.enp3s0.useDHCP = true;
+  networking.interfaces.wlp0s29u1u1.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -40,24 +46,23 @@
 
   # Select internationalisation properties.
   i18n = { defaultLocale = "de_AT.UTF-8"; };
-
   console.font = "Lat2-Terminus16";
   console.keyMap = "de";
 
   # Set your time zone.
   time.timeZone = "Europe/Vienna";
 
-  # kernel level debugging with bpf
-  programs.bcc.enable = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+
     # dl
     wget
     aria2
     youtube-dl
     ffsend
+    rclone
+    rsync
     git
     curl
     android-file-transfer
@@ -66,26 +71,59 @@
     go
     tinygo
     nixfmt
+    hlint
+    kate
+    #vscodium
+
+    # reverse engineering
+    radare2-cutter
+    ghidra-bin
+    hexdump
+
+    # office
+    pandoc
+    unoconv
+    libreoffice
+    skanlite
+    #wpsoffice
+    thunderbird
+    #discord
+    #teams
+    openconnect
+    #zoom-us
 
     # base cli
     coreutils
     man
+    file
+    traceroute
+    ranger
+    bash-completion
+    nix-bash-completions
+    direnv
     emacs
+    nano
+    tree
+    parted
     htop
     tmux
-    tree
-    file
+    networkmanager
 
     # multimedia
     cmus
     mpv
-    ffmpeg
+    vlc
+    #blender
+    gwenview
+    okular
+    irssi
     firefox
 
     # virt
     qemu
     virglrenderer
     virt-manager
+    sanoid
 
     # debug
     strace
@@ -95,105 +133,200 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  #   pinentryFlavor = "gnome3";
+  # };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  
-  # bpf debugging
+  services.openssh = {
+    enable = true;
+    hostKeys = [{
+      bits = 4096;
+      path = "/etc/ssh/ssh_host_rsa_key";
+      type = "rsa";
+    }];
+  };
+
+  # bpf kernel vm
   programs.bcc.enable = true;
 
-  # ZFS
-  services.zfs.autoScrub.enable = true;
-  services.zfs.autoScrub.interval = "monthly";
-  services.zfs.autoScrub.pools = [ "rpool" ];
-  services.zfs.trim.enable = true;
-  services.zfs.trim.interval = "weekly";
+  # lvfs firmware updater
+  services.fwupd.enable = true;
+
+  # zfs
+  services.zfs.autoScrub = {
+    enable = true;
+    interval = "monthly";
+    pools = [ "rpool" ];
+  };
+  services.zfs.trim = {
+    enable = true;
+    interval = "weekly";
+  };
+
+  # sanoid zfs snapshots
+  services.sanoid = {
+    enable = true;
+    extraArgs = [ "--verbose" ];
+    interval = "hourly";
+  };
+
+  #services.sanoid.datasets = {
+  #  "home" = {
+  #    "dataset" = rpool/home;
+  #  };
+  #};
+
+  services.sanoid.templates = {
+    "custom" = {
+      "daily" = 31;
+      "hourly" = 24;
+      "monthly" = 4;
+      "yearly" = 12;
+      "autosnap" = true;
+      "autoprune" = true;
+    };
+    "ignore" = {
+      #"monitor" = true;
+      "autosnap" = false;
+      "autoprune" = false;
+    };
+  };
+
+  services.sanoid.settings = {
+    "home" = {
+      "use_template" = "custom";
+      "dataset" = "rpool/home";
+    };
+    "root" = {
+      "use_template" = "custom";
+      "dataset" = "rpool/root/nixos";
+    };
+    "netdata" = {
+      "use_template" = "custom";
+      "dataset" = "rpool/netdata";
+    };
+    "pool" = {
+      "use_template" = "custom";
+      "dataset" = "rpool";
+    };
+    "nix" = {
+      "use_template" = "ignore";
+      "dataset" = "rpool/nix";
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = true;
+  networking.firewall = { enable = true; };
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.gutenprint pkgs.hplip ];
+  };
 
   # Enable sound.
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio = { enable = true; };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "de";
-  services.xserver.xkbOptions = "de:nodeadkeys";
+  services.xserver = {
+    enable = true;
+    layout = "at";
+    xkbVariant = "nodeadkeys";
+    xkbOptions = "eurosign:e";
+    videoDrivers = [ "nouveau" ];
+  };
 
+  # opengl
   hardware.opengl = {
     enable = true;
     driSupport32Bit = true;
     driSupport = true;
   };
 
-  #services.xserver.videoDrivers = ["amdgpu"];
-
   # Enable touchpad support.
-  services.xserver.libinput.enable = true;
+  services.xserver.libinput = {
+    enable = true;
+    horizontalScrolling = true;
+  };
 
   # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    autoNumlock = true;
+  };
+  services.xserver.desktopManager.plasma5 = {
+    enable = true;
+  };
 
   # flatpak support
   services.flatpak.enable = true;
   xdg.portal.enable = true;
 
   # steam
-  hardware.steam-hardware.enable = true;
+  hardware.steam-hardware = { enable = true; };
 
   # emacs
-  services.emacs.enable = true;
+  services.emacs = {
+    enable = true;
+    install = true;
+    defaultEditor = true;
+  };
 
   # qemu
-  virtualisation.libvirtd.enable = true;
-  virtualisation.libvirtd.qemuOvmf = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemuOvmf = true;
+  };
 
   # containers
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    storageDriver = "zfs";
+  };
 
   # nix
   nixpkgs.config.allowUnfree = true;
   nix.useSandbox = true;
 
-  # netdata system monitoring
-   services.netdata.enable = true;
-   services.netdata.config = {
-     global = {
+  # netdata monitoring
+  services.netdata = {
+    enable = true;
+    group = "netdata";
+    python.enable = true;
+
+  };
+  services.netdata.config = {
+    global = {
       "memory mode" = "dbengine";
       "cache directory" = "/var/cache/netdata";
-      "history" = "86400";
       "update every" = "2";
+      "page cache size" = "126";
       "dbengine disk space" = "2000";
       "timezone" = "Europe/Vienna";
-      };
     };
-    
-   #services.netdata.python.extraPackages = "ps: [ps.docker]";
-   services.netdata.group = "netdata";
-   services.netdata.python.enable = true;
-   
-  # firmware
-   #hardware.cpu.intel.updateMicrocode = true;
-   #hardware.cpu.amd.updateMicrocode = true;
-    hardware.enableRedistributableFirmware = true;
+  };
+  services.netdata.python.extraPackages = ps: [ ps.docker ];
 
-  
+  # firmware 
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.enableRedistributableFirmware = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.stefan = {
     isNormalUser = true;
     extraGroups = [
       "wheel"
+      "netdata"
+      "sanoid"
       "disk"
       "audio"
       "video"
@@ -204,12 +337,13 @@
     ];
   };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.autoUpgrade.enable = true;
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+
   system.stateVersion = "20.03"; # Did you read the comment?
-
+  system.autoUpgrade.enable = true;
 }
-
